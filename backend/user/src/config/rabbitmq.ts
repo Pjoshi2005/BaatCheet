a2ef81916e571ca6992ec17ef505bd64b6d64412
Pjoshi2005 <147191983+Pjoshi2005@users.noreply.github.com>
2025-11-12 21:31:19 +0530

    initial commit

---
+++ backend/user/src/config/rabbitmq.ts
@@ -0,0 +1,22 @@
+import amql from "amqplib";
+
+let channel: amql.Channel;
+
+export const connectRabbitMQ = async () => {
+  try {
+    const connection = await amql.connect({
+      protocol: "amqp",
+      hostname: process.env.Rabbitmq_Host,
+      port: 5672,
+      username: process.env.Rabbitmq_Username,
+      password: process.env.Rabbitmq_Password,
+    });
+
+    channel = await connection.createChannel();
+
+    console.log("✅ connected to rabbitmq");
+  } catch (error) {
+    console.log("Failed to connect to rabbitmq", error);
+  }
+};
+
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,5 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+import {createClient} from 'redis'
+import { connectRabbitMQ } from './config/rabbitmq.js';
 
+dotenv.config();
+
+connectDb();
+
+connectRabbitMQ()
+
+export const redisClient = createClient({
+    url : process.env.REDIS_URL!,
+})
+
+redisClient.connect().then( () =>{console.log("Redis connected successfully")}).catch(console.error)
 const app = express();
 
 const port = 5000;
